cb32b20b8a9efc4f165c093214e284e41ec03e96
Takahiro33 <mt3.mos@gmail.com>
2016-01-21 14:49:50 +0900

    campus

--- scenarios/ndn-grid-topo-plugin.cc
+++ scenarios/ndn-grid-topo-plugin.cc
@@ -3,6 +3,7 @@
 #include "ns3-dev/ns3/core-module.h"
 #include "ns3-dev/ns3/network-module.h"
 #include "ns3-dev/ns3/ndnSIM-module.h"
+#include <iostream>
 
 // Random modules
 #include <boost/foreach.hpp>
@@ -25,6 +26,8 @@
 char scenario[250] = "proactiveCaching";
 
 NS_LOG_COMPONENT_DEFINE (scenario);
+using namespace boost;
+using namespace std;
 
 namespace ns3 {
 
@@ -45,6 +48,7 @@ main(int argc, char* argv[])
 	std::string nsTFile;    	                // Name of the NS Trace file to use
 	char nsTDir[250] = "./Waypoints";           // Directory for the waypoint files
 
+	  uint32_t nStas = 1;
 
   CommandLine cmd;
   cmd.Parse(argc, argv);
@@ -56,6 +60,7 @@ main(int argc, char* argv[])
   // Install NDN stack on all nodes
   ndn::StackHelper ndnHelper;
   ndnHelper.SetForwardingStrategy("ns3::ndn::fw::BestRoute::PerOutFaceLimits", "Limit", "ns3::ndn::Limits::Window");
+  ndnHelper.SetContentStore ("ns3::ndn::cs::Freshness::Lru", "MaxSize", "256");	//trial
   ndnHelper.InstallAll();
 
   // Set BestRoute strategy
@@ -86,6 +91,14 @@ main(int argc, char* argv[])
   	if (maxSeq > 0)		consumerHelper.SetAttribute ("MaxSeq", IntegerValue(maxSeq));
   consumerHelper.Install(consumerNodes);
 
+  MobilityHelper mobility;
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                                  "Mode", StringValue ("Time"),
+                                  "Time", StringValue ("2s"),
+                                  "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"),
+                                  "Bounds", RectangleValue (Rectangle (0, 5.0,0.0, (nStas+1)*5.0)));
+       mobility.Install (consumerNodes);
+
   ndn::AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix(prefix);
   producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
@@ -98,10 +111,16 @@ main(int argc, char* argv[])
   ndn::GlobalRoutingHelper::CalculateRoutes();
 
 
-  int wnodes = 1;
+  int wnodes = 2;
 	NodeContainer wirelessContainer;
 //	wirelessContainer.Add(Names::Find<Node>("Consumer"));
-	wirelessContainer.Add(Names::Find<Node>("node1"));
+
+	for(int i = 0; i <wnodes; i++){
+		char node[10];
+		sprintf(node, "Node%d",i);
+		cout << node << endl;
+		wirelessContainer.Add(Names::Find<Node>(node));
+	}
   // Use the Wifi Helper to define the wireless interfaces for APs
   	WifiHelper wifi;
   	wifi.SetStandard(WIFI_PHY_STANDARD_80211b);
@@ -128,55 +147,61 @@ main(int argc, char* argv[])
 
   	// Add a simple no QoS based card to the Wifi interfaces
   	NqosWifiMacHelper wifiMacHelper = NqosWifiMacHelper::Default ();
-//
-//	// Create SSIDs for all the APs
-//	std::vector<Ssid> ssidV;
-//
+
+	// Create SSIDs for all the APs
+	std::vector<Ssid> ssidV;
+
 //	// We store the Wifi AP mobility models in a map, ordered by the ssid string. Will be easier to manage when
 //	// calling the modified StaMApWifiMac
 //	std::map<std::string, Ptr<MobilityModel> > apTerminalMobility;
-//
-//	for (int i = 0; i < wnodes; i++)
-//	{
-//		// Temporary string containing our SSID
-//		std::string ssidtmp("ap-" + boost::lexical_cast<std::string>(i));
-//
-//		// Push the newly created SSID into a vector
-//		ssidV.push_back (Ssid (ssidtmp));
-//
-//		// Get the mobility model for wnode i
-////		Ptr<MobilityModel> tmp = (wirelessContainer.Get (i))->GetObject<MobilityModel> ();
-//
-//		// Store the information into our map
-////		apTerminalMobility[ssidtmp] = tmp;
-//	}
-//
-//	NS_LOG_INFO ("------Assigning mobile terminal wireless cards------");
-//	NS_LOG_INFO ("------Assigning AP wireless cards------");
-//	std::vector<NetDeviceContainer> wifiAPNetDevices;
-//	for (int i = 0; i < wnodes; i++)
-//	{
-//		wifiMacHelper.SetType ("ns3::ApWifiMac",
-//						   	   "Ssid", SsidValue (ssidV[i]),
-//							   "BeaconGeneration", BooleanValue (true),
-//							   "BeaconInterval", TimeValue (Seconds (0.102)));
-//
-//		wifiAPNetDevices.push_back (wifi.Install (wifiPhyHelper, wifiMacHelper, wirelessContainer.Get (i)));
-//	}
-//
+
+	for (int i = 0; i < wnodes; i++)
+	{
+		// Temporary string containing our SSID
+		std::string ssidtmp("ap-" + boost::lexical_cast<std::string>(i));
+
+		// Push the newly created SSID into a vector
+		ssidV.push_back (Ssid (ssidtmp));
+
+		// Get the mobility model for wnode i
+//		Ptr<MobilityModel> tmp = (wirelessContainer.Get (i))->GetObject<MobilityModel> ();
+
+		// Store the information into our map
+//		apTerminalMobility[ssidtmp] = tmp;
+	}
+
+	NS_LOG_INFO ("------Assigning mobile terminal wireless cards------");
+	NS_LOG_INFO ("------Assigning AP wireless cards------");
+
 	// Create a Wifi station with a modified Station MAC.
 	wifiMacHelper.SetType ("ns3::StaWifiMac",
-//							"Ssid", SsidValue (ssidV[0]),
+							"Ssid", SsidValue (ssidV[0]),
 							"ActiveProbing", BooleanValue (true));
-//
+
 	NetDeviceContainer wifiMTNetDevices = wifi.Install (wifiPhyHelper, wifiMacHelper, Names::Find<Node>("Consumer"));
-	 wifiMTNetDevices = wifi.Install (wifiPhyHelper, wifiMacHelper, Names::Find<Node>("Node1"));
+
+	std::vector<NetDeviceContainer> wifiAPNetDevices;
+	for (int i = 0; i < wnodes; i++)
+	{
+		wifiMacHelper.SetType ("ns3::ApWifiMac",
+						   	   "Ssid", SsidValue (ssidV[i]),
+							   "BeaconGeneration", BooleanValue (true),
+							   "BeaconInterval", TimeValue (Seconds (0.102)));
+
+		wifiAPNetDevices.push_back (wifi.Install (wifiPhyHelper, wifiMacHelper, wirelessContainer.Get (i)));
+	}
+//	 wifiMTNetDevices = wifi.Install (wifiPhyHelper, wifiMacHelper, Names::Find<Node>("Node1"));
 
 	// Using the same calculation from the Yans-wifi-Channel, we obtain the Mobility Models for the
 	// mobile node as well as all the Wifi capable nodes
 
+
   Simulator::Stop(Seconds(20.0));
 
+  NodeContainer global = NodeContainer::GetGlobal ();
+  // The failure of the link connecting consumer and router will start from seconds 10.0 to 15.0
+  Simulator::Schedule(Seconds(10.0), ndn::LinkControlHelper::FailLink, global.Get(0), global.Get(1));
+  Simulator::Schedule(Seconds(15.0), ndn::LinkControlHelper::UpLink, global.Get(0), global.Get(1));
   Simulator::Run();
   Simulator::Destroy();
 
